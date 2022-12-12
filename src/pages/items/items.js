d5077d8e4657e2f16a06d82ecf202d934ddd0788
Asif <aumi.asif@gmail.com>
2022-12-12 21:16:00 +0600

    theme updated

---
+++ src/pages/items/items.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MenuItemCard from '../../components/card/menuItemCard';
+
+const Items = props => {
+  return (
+    <div>
+      <MenuItemCard />
+    </div>
+  );
+};
+
+Items.propTypes = {};
+
+export default Items;
